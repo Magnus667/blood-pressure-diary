02b9bf86b511fcabde547564e7f0aeeaec03f469
magnus <magnus.eisenhuth@y-com-automation.de>
2021-01-21 20:53:59 +0100

    Added static assets to Backend to serve Angular Frontend

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// '..' is used for heroku. '../..' is used for development.
+// TODO: Use NODE_ENV = prod for this.
+const pathFix = process.env.npm_lifecycle_event !== "start" ? '../..' : '..';
+
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, pathFix, '/apps/blood-pressure-diary')
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
