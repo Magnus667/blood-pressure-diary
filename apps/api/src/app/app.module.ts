5eece6ea9e6a14aaa95487645024dc490c4fdbc3
magnus <magnus.eisenhuth@y-com-automation.de>
2021-01-21 21:24:59 +0100

    Try fixing the path of the build

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -8,11 +8,11 @@ import { AppService } from './app.service';
 // '..' is used for heroku. '../..' is used for development.
 // TODO: Use NODE_ENV = prod for this.
 const pathFix = process.env.npm_lifecycle_event !== "start" ? '../..' : '..';
-
+console.log(__dirname, pathFix, '/blood-pressure-diary');
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, pathFix, '/apps/blood-pressure-diary')
+      rootPath: join(__dirname, pathFix, '/blood-pressure-diary')
     }),
   ],
   controllers: [AppController],
