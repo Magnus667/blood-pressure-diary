88861878e0dfaf7f608db10455ba7ad382a99816
magnus <magnus.eisenhuth@y-com-automation.de>
2021-01-23 13:13:34 +0100

    Try to connect to database again

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { Message } from '@blood-pressure-diary/api-interfaces';
 
+import  * as pg from 'pg';
+
 @Injectable()
 export class AppService {
+  constructor(){
+    // const pool = new Pool
+  }
   getData(): Message {
     return { message: 'Welcome to api!' };
   }
